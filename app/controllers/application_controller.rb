7cedf9f8ac987c02415c5d9d9c82ccf1aa36fe9a
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-14 16:17:34 +0600

    title app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# encoding:utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def title
+    base_title = "Система: ReHandy"
+    if @title
+      "#{base_title} + #{@title}"
+    else
+      base_title
+    end
+  end
+  
 end
