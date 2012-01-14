c8a2addcb904a46376b0cf7c1fc5158bf68a1a9b
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-14 16:19:41 +0600

    moved method title from app controller to app helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def title
-    base_title = "Система: ReHandy"
-    if @title
-      "#{base_title} + #{@title}"
-    else
-      base_title
-    end
-  end
-  
 end
