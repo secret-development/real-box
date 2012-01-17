3242a4e529042715591059dacbbef4a2782afb35
bek <obozrevatel90@gmail.com>
2012-01-17 12:55:26 +0600

    valid test

--- app/models/rcustomer.rb
+++ app/models/rcustomer.rb
@@ -1,2 +1,4 @@
+# encoding:utf-8
 class Rcustomer < ActiveRecord::Base
+validates :firstname, :lastname, :presence => true
 end
--- spec/models/rcustomer_spec.rb
+++ spec/models/rcustomer_spec.rb
@@ -1,5 +1,17 @@
+# encoding:utf-8
 require 'spec_helper'
 
 describe Rcustomer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @attr = {
+      :firstname => "Ivan",
+      :lastname => "Ivanov",
+      :phonehome => ""
+    }
+  end
+  it "should create a new instance with valid attributes" do
+    Rcustomer.create!(@attr)    
+  end
 end
