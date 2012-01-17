fd012a40607ceb73eeced9437abd79ea35cdd8b8
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-17 13:03:31 +0600

    edit css

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,6 +7,22 @@ $(function(){
 });
 
 // forms:
+// type deal:
 $(document).ready(function() {
   $('#type-deal').buttonset();
+});
+// price:
+$(function(){
+  $("#price").slider({
+    range: true,
+    min: 1000,
+    max: 2000000,
+    step: 1000,
+    values: [5000, 500000],
+    slide: function(event, ui){
+      $( "#price-label" ).html( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+  $( "#price-label" ).html( "$" + $( "#price" ).slider( "values", 0 ) +
+  			" - $" + $( "#price" ).slider( "values", 1 ) );
 });
