fecd02a45aff7af6be4d662a3a5c79e3c2191019
bek <obozrevatel90@gmail.com>
2012-01-18 16:35:26 +0600

    add real to customer table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120118102132) do
+ActiveRecord::Schema.define(:version => 20120118103006) do
 
   create_table "customers", :force => true do |t|
     t.string   "firstname"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120118102132) do
     t.text     "note"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "real"
   end
 
   add_index "customers", ["firstname", "lastname"], :name => "index_rcustomers_on_firstname_and_lastname"
