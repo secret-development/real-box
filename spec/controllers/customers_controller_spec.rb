ed5c97f367985a20b8ade05f994657ea076a75f0
bek <obozrevatel90@gmail.com>
2012-01-20 10:41:45 +0600

    redirect_to action 'index'

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -29,7 +29,7 @@ describe CustomersController do
     subject {post :create, :customer => {:firstname => "Ivan", :lastname => "Ivanov"}}
     
     it "redirects_to customer_url(@customer)" do
-      subject.should redirect_to(customers_url(assigns(:customer)))      
+      subject.should redirect_to(customers_url(assigns(:customer)).id)      
     end
     it "redirect_to :action 'index'" do
       subject.should redirect_to :action => :index   #:action => :show, :id => assigns(:customer).id
