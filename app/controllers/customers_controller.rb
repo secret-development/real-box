3c2cdee3d3adb3cfb72908fcc0e6c9beaa5dc01c
bek <obozrevatel90@gmail.com>
2012-01-21 13:26:16 +0600

    string typecust

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -18,7 +18,6 @@ class CustomersController < ApplicationController
   
   def show
     @customer = Customer.find(params[:id])
-    convert(@customer.real)
     respond_with @customer    
   end
   
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,4 @@
 # encoding: UTF-8
 class Customer < ActiveRecord::Base
   validates :firstname, :lastname, :presence => true
-  
-  def convert(real)
-    if @customer.real ==true
-      real = "Да"
-    else 
-      real = "Нет"
-    end       
-  end
 end
