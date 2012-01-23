076ff779229a27888706edc71cf04dc0cbebe241
bek <obozrevatel90@gmail.com>
2012-01-23 15:12:00 +0600

    error messages

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 # encoding: UTF-8
 class Customer < ActiveRecord::Base
   validates :firstname, :lastname, :presence => true
-  validates :phonehome, :phonemobile, :numericality => { :only_integer => true }
+  #validates :firstname, :lastname, :format => { :with => /^"([^"]*)"$/, :message => "Вводить только буквы!" }
+  validates :phonehome, :phonemobile, :numericality => { :only_integer => true, :message => "Только целые числа!" }
   scope :real, where(:real => true)
 end
