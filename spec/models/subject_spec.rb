d1e89f0dbe9d67ff4e470f0553dda74ed7df8819
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-23 11:51:06 +0600

    test subject model(for presence)

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -31,5 +31,41 @@ describe Subject do
     @subject = Subject.create!(@attr)
   end
   
+  it "should require a typesubject" do
+    req_presence_sub(:typesubject).should_not be_valid
+  end
   
+  it "should require a cityname" do
+    req_presence_sub(:cityname).should_not be_valid
+  end
+  
+  it "should require a township" do
+    req_presence_sub(:township).should_not be_valid
+  end
+  
+  it "should require address" do
+    req_presence_sub(:address).should_not be_valid
+  end
+  
+  it "should require price" do
+    req_presence_sub(:price).should_not be_valid
+  end
+  
+  it "should require numbofrooms" do
+    req_presence_sub(:numbofrooms).should_not be_valid
+  end
+  
+  it "should require square" do
+    req_presence_sub(:square).should_not be_valid
+  end
+  
+  it "should require floor" do
+    req_presence_sub(:floor).should_not be_valid
+  end
+
+  
+  # Require presence subject(validates :presence => true)
+  def req_presence_sub(par)
+    @subejct = Subject.new(@attr.merge(par => ""))
+  end
 end
