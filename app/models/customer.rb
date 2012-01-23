8b022520abb779947493e51c2eef8ef0b64c9280
bek <obozrevatel90@gmail.com>
2012-01-23 14:43:05 +0600

    validates integer true

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 # encoding: UTF-8
 class Customer < ActiveRecord::Base
   validates :firstname, :lastname, :presence => true
+  validates :phonehome, :phonemobile, :numericality => { :only_integer => true }
   scope :real, where(:real => true)
 end
