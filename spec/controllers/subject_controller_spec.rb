9f0edac94537be53ec49c4563248e4609cb94eda
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-23 12:47:24 +0600

    get index(subject controller spec)

--- spec/controllers/subject_controller_spec.rb
+++ spec/controllers/subject_controller_spec.rb
@@ -1,5 +1,13 @@
+# encoding: UTF-8
 require 'spec_helper'
 
 describe SubjectController do
-
+  render_views
+  
+  describe "GET 'index'" do
+    it "should be success" do
+      get :index
+      response.should be_success
+    end
+  end
 end
