0169573ffdc310a78269cb2bf3da7febd37bc5ee
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-25 17:52:29 +0600

    errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,5 @@
 # encoding:utf-8
 module ApplicationHelper
-  
-  # cities
-  KOSTANAY = [ "Костанай", "Лисаковск", "Рудный", "Житикара"]
-  CURRENT_CITY = KOSTANAY
-  
-  # type subjects:
-  TS = ["Квартира", "Коттедж", "Комната", "Офис", "Дача"]
-  
-  # type structure:
-  TYPESTR = ["Кирпичное", "Панельное", "Шлакоблочное", "Монолитное", "Деревянное", "Неизвестно"]
-  
-  # YEAR OF CONSTRUCTIONS
-  current_year = Time.now.year
-  YEAROFCONSTR = (1900..current_year).to_a
-  
-  # YES OR NOW
-  YOR = ["Да", "Нет", "Неизвестно"]
-  
-  # Balcony
-  BALCONY = ["Балкон","Лоджия","Нет","Неизвестно"]
-  
-  # WC
-  WC = ["Раздельный","Совмещенный","Неизвестно"]
-
-  # LAYOT
-  LAYOUT = ["\"Сталинка\"", "\"Хрущевка\"", "Улучшенная планировка", 
-            "\"Новая\" планировка", "Другая"]
-
- # STATE
- STATE = ["Евроремонт","Идеальное","Хорошее","После строительства",
-          "Требуется ремонт", "Удовлетворительное"]
-          
-          
           
   def title
     base_title = "Система: ReHandy"
@@ -43,6 +10,8 @@ module ApplicationHelper
     end
   end
   
+  def error_messages_for(object)
+    render(:partial => "shared/error_messages", :locals => {:object => object})
+  end
+  
 end
-
-# todo: сделать отдельно таблицу для районов которые будут привязанны к городам
