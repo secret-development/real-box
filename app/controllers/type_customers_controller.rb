ff343fc89a951c950497b6144f32951d317fce4d
bek <obozrevatel90@gmail.com>
2012-01-26 22:49:47 +0600

    create index, new methods for type_customers

--- app/controllers/type_customers_controller.rb
+++ app/controllers/type_customers_controller.rb
@@ -1,3 +1,11 @@
 # encoding: UTF-8
 class TypeCustomersController < ApplicationController
+  respond_to :html
+  
+  def index    
+  end
+  
+  def new
+    
+  end
 end
--- spec/controllers/type_customers_controller_spec.rb
+++ spec/controllers/type_customers_controller_spec.rb
@@ -3,5 +3,19 @@ require 'spec_helper'
 
 describe TypeCustomersController do
   render_views
+  
+  before(:each) do
+    @typecustomer = Factory(:type_customer)
+  end
+  
+  it "GET 'index'" do
+    get :index
+    response.should be_success    
+  end
+  
+  it "GET new" do
+    get :new
+    response.should be_success    
+  end
 
 end
