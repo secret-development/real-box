092bc450dd3d0ef6346196d8a3b1d73da9c30039
Yerlan Kussainov <lagox.mail@gmail.com>
2012-01-27 15:11:06 +0600

    prepare for ValueField controller spec

--- spec/controllers/value_fields_controller_spec.rb
+++ spec/controllers/value_fields_controller_spec.rb
@@ -1,5 +1,17 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe ValueFieldsController do
+  
+  before(:each) do
+    @conditionfield = Factory(:condition_field)
+    @valuefield = Factory(:value_field)
+    @attr = {
+      :valuefield => "Нет",
+      :condition_field_id => @conditionfield.id
+    }
+  end
 
+  
+  
 end
