f2c619b5b0f43804aca301d6f9bda30b37a6e8bf
bek <obozrevatel90@gmail.com>
2012-01-27 14:57:28 +0600

    assotions socialstatus

--- spec/models/social_status_spec.rb
+++ spec/models/social_status_spec.rb
@@ -10,4 +10,9 @@ describe SocialStatus do
   it "should be create" do
     SocialStatus.create!(@attr)    
   end
+  
+  it "should respond to customer" do
+    t = SocialStatus.new
+    t.should respond_to(:customers)    
+  end
 end
