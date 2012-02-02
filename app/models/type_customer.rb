90ab9f26a56b072a6edf1b468dd24f1f707a9448
bek <obozrevatel90@gmail.com>
2012-02-02 15:03:03 +0600

    uniqueness typecustomer and css index customr

--- app/models/type_customer.rb
+++ app/models/type_customer.rb
@@ -1,7 +1,7 @@
 # encoding: UTF-8
 class TypeCustomer < ActiveRecord::Base
   has_many :customers, :dependent => :destroy
-  validates :title, :presence => true
+  validates :title, :presence => true, :uniqueness => true
   default_scope order('title ASC')
   def button_value
     if new_record?
