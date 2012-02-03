0aaabe545bae6e405bb372e4fb5f4d10f01fc5ab
bek <obozrevatel90@gmail.com>
2012-02-03 17:43:31 +0600

    bug

--- spec/factories.rb
+++ spec/factories.rb
@@ -31,6 +31,10 @@ Factory.define :customer do |customer|
   customer.note           "bla-bla-bla"
 end
 
-Factory.define :type_customer_with_customer, :parent => :type_customer do |type|
-  type.after_create { |a| Factory(:customer, :type_customer => a)}  
-end
+#Factory.define :type_customer_with_customer, :parent => :type_customer do |type|
+#  type.after_create { |a| Factory(:customer, :type_customer => a)}  
+#end
+
+#Factory.define :social_status_with_customer, :parent => :social_status do |social|
+#  social.after_create { |b| Factory(:customer, :social_status => b)}  
+#end
