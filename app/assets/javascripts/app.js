3ab06391df361037eddb6bc42cae91374f463a4e
bek <obozrevatel90@gmail.com>
2012-02-07 17:14:14 +0600

    plugin validation for customers

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
   $(".cancel-form").click(function(){
     javascript:history.go(-1);
   });
+  
+  // validation customer
+//  $("#validation").validate({
+//  rules:{
+//  "customer[lastname]":{required:true},
+//  "customer[firstname]":{required:true},
+//  "customer[phonehome]":{required:false,number:true},
+//  "customer[phonemobile]":{required:false,number:true},
+//  "customer[email]":{email:true},
+//  }});
 });
