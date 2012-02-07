acccefb0ecb55ecdf30b2e37c25a68ee583851b1
bek <obozrevatel90@gmail.com>
2012-02-07 18:08:10 +0600

    generate kaminari views

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,7 +3,7 @@ class CustomersController < ApplicationController
   respond_to :html
   
   def index
-    @customers = Customer.real.all
+    @customers = Customer.real.page(params[:page]).per(14)
     @title = "Клиенты"
     #@potentials = Customer.potentials.all
   end
