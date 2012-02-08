2821210d3aa1d68e21f4f257e43ddc865556b590
bek <obozrevatel90@gmail.com>
2012-02-08 12:43:05 +0600

    potentials link and redirect

--- app/controllers/potentials_controller.rb
+++ app/controllers/potentials_controller.rb
@@ -6,4 +6,11 @@ class PotentialsController < ApplicationController
     @potentials = Customer.potentials.all
     @title = "Потенциальные клиенты"
   end
+  
+  def destroy
+    @potentials = Customer.find(params[:id])
+    @potentials.destroy
+    flash[:notice] = "Клиент успешно удален"
+    redirect_to @potentials    
+  end
 end
