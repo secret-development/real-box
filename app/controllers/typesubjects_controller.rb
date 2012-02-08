73833f956a2573c5ec80b9fa497e942472e96d8e
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-08 13:03:44 +0600

    add code to TypesubjectsController and test it

--- app/controllers/typesubjects_controller.rb
+++ app/controllers/typesubjects_controller.rb
@@ -1,2 +1,42 @@
+# encoding: utf-8
 class TypesubjectsController < ApplicationController
+
+  respond_to :html
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def edit
+  end
+  
+  def create
+    @typesubject = Typesubject.new(params[:typesubject])
+    if @typesubject.save
+      flash[:notice] = "Тип объекта успешно добавлен"
+      respond_with(@typesubject, :location => typesubjects_path)
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @typesubject = Typesubject.find(params[:id])
+    if @typesubject.update_attributes(params[:typesubject])
+      flash[:notice] = "Тип объекта успешно обновлён"
+      respond_with(@typesubject, :location => typesubjects_path)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @typesubject = Typesubject.find(params[:id])
+    @typesubject.destroy
+    flash[:notice] = "Тип объекта успешно удалён"
+    redirect_to typesubjects_path
+  end
+  
 end
