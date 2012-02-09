59f57d6f2ee8d7b125fbef774a8a8ef113cda2e8
bek <obozrevatel90@gmail.com>
2012-02-09 14:31:43 +0600

    add link sorter

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
   //сортировка таблицы
-  $('.customer-sort').tablesorter({
-    widgets: ["zebra"],
-    sortList: [[1,0]]
-    });
+  $('.customer-sort').tablesorter({sortList: [[1,0]]});
   
+  //sort lastname
+  $('#lastname-link').click(function() {
+    var sorting = [[1,0]];
+    $('.customer-sort').trigger("sorton", [sorting]);
+    return false;
+  });
+  
+  //sort typecustomer
+  $('#type-link').click(function() {
+    var sorting = [[3,0]];
+    $('.customer-sort').trigger("sorton", [sorting]);
+    return false;
+  });  
 });
