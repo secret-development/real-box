3a1a2c04375da252f7af8811bd62e86dd350b2f8
bek <obozrevatel90@gmail.com>
2012-02-09 15:33:50 +0600

    delete plugin tablesorter and pager

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
   //сортировка таблицы
-  $('.customer-sort')
-    .tablesorter({sortList: [[1,0]]})
-    //постраничное разбиение
-    .tablesorterPager({container: $("#pager")});
   
-  // Сортировка при помощи ссылок
-  //сортировка по фамилии
-  $('#lastname-link').click(function() {
-    var sorting = [[1,0]];
-    $('.customer-sort').trigger("sorton", [sorting]);
-    return false;
-  });
-  //сортировка по типам клиентов
-  $('#type-link').click(function() {
-    var sorting = [[3,0]];
-    $('.customer-sort').trigger("sorton", [sorting]);
-    return false;
-  });  
 });
--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,7 +1,3 @@
 # encoding: UTF-8
 module CustomersHelper
-  
-  def next_page
-    raw("<i class='icon-chevron-right'></i>")
-  end
 end
