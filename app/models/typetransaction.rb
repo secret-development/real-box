826a68d1d6e17850669883af64966d0ebc810a4f
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-13 18:52:02 +0600

    spec for transactions

--- app/models/typetransaction.rb
+++ app/models/typetransaction.rb
@@ -1,2 +1,3 @@
 class Typetransaction < ActiveRecord::Base
+  has_many :customers
 end
--- spec/models/typetransaction_spec.rb
+++ spec/models/typetransaction_spec.rb
@@ -1,5 +1,28 @@
+# encoding:utf-8
 require 'spec_helper'
 
 describe Typetransaction do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @typetransaction = Factory(:typetransaction)
+    
+    # valid data
+    @attr = {
+      :name => "Обмен"
+    }
+  end
+  
+  describe "associations" do
+    describe "has_many :customers" do
+      it "should respond_to customers" do
+        @typetransaction = Typetransaction.new(@attr)
+        @typetransaction.should respond_to(:customers)
+      end
+      
+      it "should has_many :customers" do
+        t = Typetransaction.reflect_on_association(:customers)
+        t.macro.should == :has_many
+      end
+      
+    end
+  end
 end
