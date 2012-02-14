1c9dd763a15d568e9abf971236843ca72da11361
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-14 16:23:18 +0600

    views for subject

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -9,6 +9,10 @@ class Subject < ActiveRecord::Base
   validates :typesubject_id, :presence => true
   validates :city_id, :presence => true
   validates :price, :presence => true, :numericality => true
+  
+  def legend_value
+    new_record? ? "Добавить объект" : "Редактировать объект"
+  end
 end
 
 # == Schema Information
