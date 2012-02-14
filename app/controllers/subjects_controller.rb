2899ddd54946c1ba6e6b39fc7ae7e66ac08f4568
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-14 14:59:48 +0600

    subjects_controller spec

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,52 @@
+# encoding:utf-8
 class SubjectsController < ApplicationController
+  respond_to :html
+  
+  def index
+    @subjects = Subject.all
+    respond_with(@subjects)
+  end
+  
+  def show
+    @subject = Subject.find(params[:id])
+    respond_with(@subject)
+  end
+
+  def new
+    @subject = Subject.new
+    respond_with(@subject)
+  end
+  
+  def edit
+    @subject = Subject.find(params[:id])
+    respond_with(@subject)
+  end
+  
+  def create
+    @subject = Subject.new(params[:subject])
+    if @subject.save
+      flash[:notice] = "Объект успешно создан"
+      respond_with(@subject, :location => @subject)
+    else
+      render 'new'
+    end
+  end
+  
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(params[:subject])
+      flash[:notice] = "Объект успешно обновлён"
+      respond_with(@subject, :location => @subjects)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "Объект успешно удалён"
+    redirect_to subjects_path
+  end
+
 end
--- spec/controllers/subjects_controller_spec.rb
+++ spec/controllers/subjects_controller_spec.rb
@@ -1,5 +1,157 @@
+# encoding:utf-8
 require 'spec_helper'
 
 describe SubjectsController do
-
+  render_views
+  
+  before(:each) do
+    city = Factory(:city)
+    typesubject = Factory(:typesubject)
+    @subject = Factory(:subject, :typesubject => typesubject, :city => city)
+  end
+  
+  it "get 'index'" do
+    get :index
+    response.should be_success
+  end
+  
+  it "get new" do
+    get :new
+    response.should be_success
+  end
+  
+  it "get edit" do
+    get :edit, :id => @subject.id
+    response.should be_success
+  end
+  
+  it "get show" do
+    get :show, :id => @subject.id
+    response.should be_success
+  end
+  
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = invalid_data
+      end
+      
+      it "should render the new page" do
+        post :create, :subject => @attr
+        response.should render_template('new')
+      end
+      
+      it "should not create a subject" do
+        lambda do
+          post :create, :subject => @attr
+        end.should_not change(Subject, :count)
+      end
+    end
+    
+    describe "success" do
+      before(:each) do
+        @attr = valid_data
+      end
+      
+      it "should redirect to show page" do
+        post :create, :subject => @attr
+        response.should redirect_to(subject_path(assigns(:subject)))
+      end
+      
+      it "should create a subject" do
+        lambda do
+          post :create, :subject => @attr
+        end.should change(Subject, :count).by(1)
+      end
+      
+      it "should have a success message" do
+        post :create, :subject => @attr
+        flash[:notice].should =~ /Объект успешно создан/i
+      end
+    end
+  end
+  
+  
+  describe "PUT 'update'" do
+    describe "failure" do
+      before(:each) do
+        @attr = invalid_data
+      end
+      
+      it "should render the edit page" do
+        put :update, :id => @subject, :subject => @attr
+        response.should render_template('edit')
+      end
+      
+      it "should not create a subject" do
+        lambda do
+          put :update, :id => @subject, :subject => @attr
+        end.should_not change(Subject, :count)
+      end
+    end
+    
+    describe "success" do
+      before(:each) do
+        @attr = valid_data
+      end
+      
+      it "should redirect to show page" do
+        put :update, :id => @subject, :subject => @attr
+        response.should redirect_to(subject_path(assigns(:subject)))
+      end
+      
+      it "should not create a subject" do
+        lambda do
+          put :update, :id => @subject, :subject => @attr
+        end.should_not change(Subject, :count)
+      end
+      
+      it "should have success message" do
+        put :update, :id => @subject, :subject => @attr
+        flash[:notice].should =~ /Объект успешно обновлён/i
+      end
+    end
+  end
+  
+  describe "destroy" do
+    it "should redirect_to subjects list page" do
+      delete :destroy, :id => @subject
+      response.should redirect_to(subjects_path)
+    end
+    
+    it "should destroy the subject" do
+      lambda do
+        delete :destroy, :id => @subject
+      end.should change(Subject, :count).by(-1)
+    end
+    
+    it "should have success message" do
+      delete :destroy, :id => @subject
+      flash[:notice].should =~ /Объект успешно удалён/i
+    end
+  end
+  
+  
+  # valid data
+  def valid_data
+    {
+      :typesubject_id => 1,
+      :city_id => 2,
+      :price => 100003,
+      :area => 80,
+      :address => "Баймагамбетова 15, 23"
+    }
+  end
+  
+  # invalid data
+  def invalid_data
+    {
+      :typesubject_id => 2,
+      :city_id => nil,
+      :price => "swsws",
+      :area => 80,
+      :address => "Баймагамбетова 15, 23" 
+    }  
+  end
+  
 end
