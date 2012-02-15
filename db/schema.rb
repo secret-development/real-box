d2ae85f72573fb0160b3f01cee63ea8d7205189d
Mons Writer <monswriter@gmail.com>
2012-02-15 17:12:04 +0600

    modified views, some helper methods and tests for tasks and users (formatted date)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120214115425) do
 
   create_table "cities", :force => true do |t|
@@ -35,10 +34,12 @@ ActiveRecord::Schema.define(:version => 20120214115425) do
     t.string   "phonemobile"
     t.string   "email"
     t.text     "note"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.boolean  "potentials",         :default => false
+    t.integer  "type_customer_id"
     t.integer  "social_status_id"
+    t.string   "permalink"
     t.integer  "typetransaction_id"
   end
 
@@ -68,6 +69,12 @@ ActiveRecord::Schema.define(:version => 20120214115425) do
     t.boolean  "done"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "type_customers", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "typesubjects", :force => true do |t|
