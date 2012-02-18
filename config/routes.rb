78642723fe9b069f3a44bc0d5343c6159dd39885
bek <obozrevatel90@gmail.com>
2012-02-18 13:04:09 +0600

    delete route typecustomer

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Crm::Application.routes.draw do
   resources :subjects
   
   resources :cities, :except => [:show]
-  resources :type_customers, :except => [:show]
   resources :social_statuses, :except => [:show]
   resources :condition_fields
   resources :value_fields, :except => [:show]
