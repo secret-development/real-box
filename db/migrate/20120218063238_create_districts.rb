a5e5cade63f52ecd9dd2ae89731121027aa56d60
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-18 12:42:56 +0600

    edit migrations

--- db/migrate/20120218063238_create_districts.rb
+++ db/migrate/20120218063238_create_districts.rb
@@ -3,8 +3,8 @@ class CreateDistricts < ActiveRecord::Migration
     create_table :districts do |t|
       t.integer :city_id
       t.string :title
-      t.index :title
       t.timestamps
     end
+    add_index :districts, :title
   end
 end
