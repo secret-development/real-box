f6b6f9fff72c2d36917ed05b1ff0f9c2b6119400
bek <obozrevatel90@gmail.com>
2012-02-24 13:38:39 +0600

    add seeds 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,5 +21,5 @@ statuses_of_transactions.each do |s|
   Statustransaction.create(:name => s)
 end
 
-User.create(email: 'admin@example.com', password: 'password')
+User.create(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
 
