55c41ea7557f6ca6d59fad55d40544b328511af4
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-24 12:37:57 +0600

    edit PhotoUploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -22,6 +22,18 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def default_url
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+  before :store, :remember_cache_id
+  after :store, :delete_tmp_dir
+  
+  def remember_cache_id(new_file)
+    @cache_id_was = cache_id
+  end
+
+  def delete_tmp_dir(new_file)
+    if @cache_id_was.present? && @cache_id_was =~ /\A[\d]{8}\-[\d]{4}\-[\d]+\-[\d]{4}\z/
+      FileUtils.rm_rf(File.join(cache_dir, @cache_id_was))
+    end
+  end
 
   # Process files as they are uploaded:
   process :resize_to_fill => [800, 600]
