850cff4f71699e8ffd242bfb586496d96b7f993f
Yerlan Kussainov <lagox.mail@gmail.com>
2012-02-25 17:55:05 +0600

    edit tasks_controller_spec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -6,7 +6,6 @@ describe TasksController do
   render_views
 
   before(:each) do
-    controller.stub!(:current_user)
     controller.stub!(:all_deny)
     @user = Factory(:user)
     @task = Factory(:task, :user => @user)
