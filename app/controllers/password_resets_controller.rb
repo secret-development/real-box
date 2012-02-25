3b1aa87f346fa659d86d43dcbe3478e9f151199e
bek <obozrevatel90@gmail.com>
2012-02-25 14:21:02 +0600

    mailer and other

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,4 +9,22 @@ class PasswordResetsController < ApplicationController
     redirect_to sign_in_path
     flash[:notice] = "Пароль отправлен на ваш почтовый ящик"    
   end
+  
+  def edit
+    @user = User.find_by_password_reset_token!(params[:id])    
+  end
+  
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path
+      flash[:notice] = "Время истекло"
+    elsif
+      @user.update_attributes(params[:user])
+      redirect_to sign_in_path
+      flash[:notice] = "Пароль изменен"
+    else
+      render 'edit'
+    end    
+  end
 end
