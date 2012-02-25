f0090d786678d8dcd44278d36ac8e034fdf372d2
bek <obozrevatel90@gmail.com>
2012-02-25 13:26:47 +0600

    create actions

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,12 @@
+# encoding:utf-8
 class PasswordResetsController < ApplicationController
   def new
+  end
+  
+  def create
+    user = User.find_by_email(params[:email])
+    user.send_password_reset if user
+    redirect_to sign_in_path
+    flash[:notice] = "Пароль отправлен на ваш почтовый ящик"    
   end
 end
