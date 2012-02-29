1286c082b208430bdff88ab9328b0df777a302d6
bek <obozrevatel90@gmail.com>
2012-02-29 16:27:57 +0600

    spec post create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,3 +1,4 @@
+# encoding:utf-8
 require 'spec_helper'
 
 describe UsersController do
@@ -7,13 +8,53 @@ describe UsersController do
     @user = Factory(:user)
     test_log_in(@user)
     # end auth
+    @new_user = { 
+                :lastname => "Горбунов",
+                :firstname => "Анатолий",
+                :email => "gor@mail.ru",
+                :password => "password",
+                :password_confirmation => "password"
+    }
   end
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
+  describe "returns http success" do
+    it "GET 'new'" do
+      get :new
       response.should be_success
     end
+    
+    it "GET 'edit'" do
+      get :edit, :id => @user.id
+      response.should be_success      
+    end
+    
+    it "GET 'show'" do
+      get :index
+      response.should be_success      
+    end
   end
+  
+  #post create
+  describe "POST 'create'" do
+    
+    describe "success" do      
+      it "create" do
+        lambda do
+          post :create, :user => @new_user
+        end.should change(User, :count).by(1) 
+      end 
+      
+      it "should redirect to users_path" do
+        post :create, :user => @new_user
+        response.should redirect_to(users_path)        
+      end
+      
+      it "should have a successful message" do
+        post :create, :user => @new_user
+        flash[:notice].should =~ /Сотрудник создан/i        
+      end     
+    end  
+  end
+  
 
 end
