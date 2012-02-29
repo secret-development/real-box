26efd609b261358838299721f616547173065382
bek <obozrevatel90@gmail.com>
2012-02-29 13:21:37 +0600

    refactor test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,42 +12,57 @@ describe User do
     }
   end 
   
-  it "should create user" do
-    User.create!(@attr)  
-  end
-  
-  it "should require email" do
-    null_email = User.new(@attr.merge(:email => ""))
-    null_email.should_not be_valid    
-  end
-  
-  it "should accept valid email" do
-    mail = %w[man@mail.ru Woman@mail.ru]
-    mail.each do |mail|
-      valid_email = User.new(@attr.merge(:email => mail))
-      valid_email.should be_valid
-    end    
-  end
-  
-  it " should invalid email" do
-    mail = %w[man@ru woman@mail,ru]
-    mail.each  do |mail|
-      invalid_email = User.new(@attr.merge(:email => mail))
-      invalid_email.should_not be_valid
-    end    
-  end
-  
-  it "should reject duplicate email" do
-    User.create(@attr)
-    duplicate_email = User.new(@attr)
-    duplicate_email.should_not be_valid    
-  end
-  
-  it "should reject duplicate upcase email" do
-    upcase_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcase_email))
-    user_duplicate_email = User.new(@attr)
-    user_duplicate_email.should_not be_valid
+  # validation invalid
+  describe "invalid validation" do
     
+    it "should require email" do
+      null_email = User.new(@attr.merge(:email => ""))
+      null_email.should_not be_valid    
+    end
+    
+    it " should invalid email" do
+      mail = %w[man@ru woman@mail,ru]
+      mail.each  do |mail|
+        invalid_email = User.new(@attr.merge(:email => mail))
+        invalid_email.should_not be_valid
+      end    
+    end
+    
+    it "should reject duplicate email" do
+      User.create(@attr)
+      duplicate_email = User.new(@attr)
+      duplicate_email.should_not be_valid    
+    end
+    
+    it "should reject duplicate upcase email" do
+      upcase_email = @attr[:email].upcase
+      User.create!(@attr.merge(:email => upcase_email))
+      user_duplicate_email = User.new(@attr)
+      user_duplicate_email.should_not be_valid    
+    end
+  
+    it "should require lastname" do
+      not_lastname = User.new(@attr.merge(:lastname => ""))
+      not_lastname.should_not be_valid    
+    end
+      
   end
+  
+  # success validation
+  describe "success validation" do
+      
+    it "should create user" do
+      User.create!(@attr)  
+    end
+    
+    it "should accept valid email" do
+      mail = %w[man@mail.ru Woman@mail.ru]
+      mail.each do |mail|
+        valid_email = User.new(@attr.merge(:email => mail))
+        valid_email.should be_valid
+      end    
+    end  
+  
+  end
+    
 end
