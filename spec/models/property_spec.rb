a15c07b7234dbc99dfea6ea72e4a29b3d34bb21f
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-01 11:36:11 +0600

    specs for property model

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,48 @@
+# encoding:utf-8
 require 'spec_helper'
 
 describe Property do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @city = Factory(:city)
+    @typesubject = Factory(:typesubject)
+    @district = Factory(:district)
+    @customer = Factory(:customer)
+    @subject = Factory(:subject, :typesubject => @typesubject, :city => @city, 
+        :customer => @customer, :district => @district)
+        
+    @attr = {
+      :condition => "Интернет",
+      :value => "Есть",
+      :subject_id => @subject.id
+    }
+  end
+  
+  describe "validations" do
+    it "should subject require" do
+      property = Property.new(@attr.merge(:subject_id => nil))
+      property.should_not be_valid
+    end
+    
+    it "should condition require" do
+      property = Property.new(@attr.merge(:condition => nil))
+      property.should_not be_valid
+    end
+    
+    it "should value require" do
+      property = Property.new(@attr.merge(:value => nil))
+      property.should_not be_valid
+    end
+  end
+  
+  describe "associations" do
+    it "should respond_to subject" do
+      property = Property.new(@attr)
+      property.should respond_to(:subject)
+    end
+    
+    it "should belongs_to subject" do
+      property = Property.reflect_on_association(:subject)
+      property.macro.should == :belongs_to
+    end
+  end
 end
