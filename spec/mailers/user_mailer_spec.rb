26a933525fc47d655ee8f629729f5d9496049bd6
bek <obozrevatel90@gmail.com>
2012-03-01 09:41:51 +0600

    fix user mailer test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -10,7 +10,7 @@ describe UserMailer do
       mail.subject.should eq("Password Reset")
       mail.to.should eq([user.email])
       mail.from.should eq(["from@example.com"])
-      mail.body.encoded.should match(edit_password_reset_path(user.password_reset_token))
+      mail.body.encoded.should match(edit_password_resets_path(user.password_reset_token))
     end
   end
 
