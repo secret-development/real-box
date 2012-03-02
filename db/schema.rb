b7ec79916c0c0347d3622424c2034983fc4e0aa0
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-02 20:19:00 +0600

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -101,7 +101,6 @@ ActiveRecord::Schema.define(:version => 20120301090048) do
     t.datetime "updated_at",  :null => false
   end
 
-
   create_table "transactions", :force => true do |t|
     t.string   "name"
     t.text     "description"
@@ -114,7 +113,6 @@ ActiveRecord::Schema.define(:version => 20120301090048) do
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
   end
-
 
   create_table "typesubjects", :force => true do |t|
     t.string   "name"
