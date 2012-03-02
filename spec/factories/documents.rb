8478e4df0937d32abd656fc0040e5f068585175d
Mons Writer <monswriter@gmail.com>
2012-03-02 17:39:30 +0600

    modified documents.rb(factory)

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -1,8 +1,8 @@
+#encdoing: UTF-8
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :document do
-    transaction_id 1
-    doc "MyString"
+    doc "MyDoc.doc"
   end
 end
