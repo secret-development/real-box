94a98bfd5b1c343ce0bf609d77e891b954a8a1bd
Mons Writer <monswriter@gmail.com>
2012-03-02 19:34:24 +0600

    modified app.js and document_uploader.rb

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -40,16 +40,16 @@ class DocumentUploader < CarrierWave::Uploader::Base
   
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_fill => [800, 600]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fit => [50, 50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
