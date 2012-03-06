2034359347de6c9f2afe1697f5970c7288586feb
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-06 17:48:27 +0600

    delete seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,12 +14,5 @@ types_of_transactions.each do |t|
   Typetransaction.create(:name => t)
 end
 
-#status of transaction:
-Statustransaction.delete_all
-statuses_of_transactions = ['Активна', 'Отложена', 'Завершена']
-statuses_of_transactions.each do |s|
-  Statustransaction.create(:name => s)
-end
-
 User.create(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password', :role => true)
 
