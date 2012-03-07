87a6bb14b50ae5c9c06dfac17a4407f6a2de9a11
Mons Writer <monswriter@gmail.com>
2012-03-07 16:23:32 +0600

    modified transactions_controller - added exsception in load_and_authorize_resource

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -5,7 +5,7 @@ class TransactionsController < ApplicationController
   respond_to :html
   before_filter :all_deny
   helper_method :sort_column, :sort_direction
-  load_and_authorize_resource
+  load_and_authorize_resource :except => [:new, :create, :update]
   
   def new
     begin
