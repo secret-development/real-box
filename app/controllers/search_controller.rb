270d35b085193b1574dccd50a825579f0af55d6f
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-11 20:26:26 +0600

    modified search files/edit readme

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,9 @@ class SearchController < ApplicationController
   before_filter :all_deny
   
   def index
-    
+    @title = "Поиск"
+    @search = Subject.search(params[:search])
+    @subjects = @search.all
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,9 @@ Crm::Application.routes.draw do
   resources :typesubjects, :except => [:show]
   # settings
   match "settings" => "settings#index", :as => :settings
+  
+  # search
+  resources :search
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
