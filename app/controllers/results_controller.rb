5bc4217aba0651b384d9d3b6d8bc9970be26d875
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-11 23:58:08 +0600

    start search

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,10 @@
 # -*- encoding : utf-8 -*-
 class ResultsController < ApplicationController
+  before_filter :all_deny
   
   def index
-    @subjects = Subject.where{address =~ '%Пушкин%'}
+    @search = Subject.search(params[:search])
+    @subjects = @search.all
   end
   
 end
