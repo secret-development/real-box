0d8ffeec7ffdb061f8edb83a928bf6d94eba548d
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-11 15:20:55 +0600

    edit routes(root)

--- config/routes.rb
+++ config/routes.rb
@@ -92,7 +92,7 @@ Crm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#index'
+  root :to => 'search#index'
 
   # See how all your routes lay out with "rake routes"
 
