f89a6a91598d8819f84a7ba91b455b2ac7c4b885
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-12 00:30:22 +0600

    districts finish

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,6 +3,7 @@ class ResultsController < ApplicationController
   before_filter :all_deny
   
   def index
+    @title = "Поиск"
     @search = Subject.search(params[:search])
     @subjects = @search.all
   end
