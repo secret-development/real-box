7c847c87951728dbf3fe8cb6149b74bcf5f3ed78
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-11 15:34:05 +0600

    edit search controller spec(add auth)

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -4,6 +4,13 @@ require 'spec_helper'
 describe SearchController do
   render_views
   
+  before(:each) do
+    # start auth
+    @user = Factory(:user)
+    test_log_in(@user)
+    # end auth
+  end
+  
   it "GET index should be success" do
     get :index
     response.should be_success
