910f24cdca5cb64b5c5ab6f22a7a8ab6b2b12b60
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-12 13:51:51 +0600

    style search

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,11 +1,20 @@
 # -*- encoding : utf-8 -*-
 class ResultsController < ApplicationController
   before_filter :all_deny
+  respond_to :html
   
   def index
     @title = "Поиск"
     @search = Subject.search(params[:search])
-    @subjects = @search.all
+    if params[:search]
+      @subjects = @search.page(params[:page]).per(page_paginate)
+      respond_with(@subjects)
+    end
   end
   
+  private
+  
+    def page_paginate
+      15
+    end
 end
