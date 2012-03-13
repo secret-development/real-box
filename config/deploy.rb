dee07f0861d04705399f0dd861f17d9314d4658b
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-13 15:49:16 +0600

    edit deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,8 +77,6 @@ namespace :deploy do
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    # run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
-    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
-    run unicorn_start_cmd
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 end
