16f5481ad2618919756082507a96ba6846bc3c6d
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-14 11:27:14 +0600

    edit help, and fix

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -4,9 +4,9 @@ class HelpController < ApplicationController
   before_filter :all_deny
   respond_to :html
   
-  def index
-    @title = "Справка"
-  end
+  # def index
+  #   @title = "Справка"
+  # end
   
   def search
     @title = "Справка | Поиск"
