5aeea1a694e88c64d476c102f062925591151ae6
Yerlan Kussainov <lagox.mail@gmail.com>
2012-03-29 17:58:48 +0600

    fix, css styleing

--- app/controllers/potentials_controller.rb
+++ app/controllers/potentials_controller.rb
@@ -24,11 +24,11 @@ class PotentialsController < ApplicationController
   private
   
   def sort_column
-    Customer.column_names.include?(params[:sort]) ? params[:sort] : "lastname"    
+    Customer.column_names.include?(params[:sort]) ? params[:sort] : "id"    
   end
   
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"    
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"    
   end
   
 end
