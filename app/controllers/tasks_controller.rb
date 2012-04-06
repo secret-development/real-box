bf4aa26b5996d59d5f7c92262681945f641fb8dc
Mons Writer <monswriter@gmail.com>
2012-04-06 16:45:11 +0600

    modified tasks_controller - added titles for adm and usr methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,12 +17,12 @@ class TasksController < ApplicationController
   
   def adm
     @tasks = Task.adm_tasks.search(params[:search]).order(sort_column + " " + sort_direction).page(params[:page]).per(page_paginate)
-    @title = "Задачи"
+    @title = "Задачи, поставленные мной"
   end
   
   def usr
     @tasks = Task.usr_tasks.search(params[:search]).order(sort_column + " " + sort_direction).page(params[:page]).per(page_paginate)
-    @title = "Задачи"
+    @title = "Задачи персонала"
   end
 
   def show
