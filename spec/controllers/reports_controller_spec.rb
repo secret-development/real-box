a279663c5445d3c5868979d9bb3475446374d8f9
Yerlan Kussainov <lagox.mail@gmail.com>
2012-04-06 13:52:06 +0600

    edit reports spec

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -11,7 +11,7 @@ describe ReportsController do
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
-      response.should be_success
+      response.should redirect_to(finance_reports_path)
     end
   end
 
