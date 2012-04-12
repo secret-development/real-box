308abec1d3c8f113c760468f39641ed762dcd823
Yerlan Kussainov <lagox.mail@gmail.com>
2012-04-12 18:23:53 +0600

    edit schema

--- db/schema.rb
+++ db/schema.rb
@@ -37,9 +37,7 @@ ActiveRecord::Schema.define(:version => 20120406075403) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "potentials",         :default => false
-    t.integer  "type_customer_id"
     t.integer  "social_status_id"
-    t.string   "permalink"
     t.integer  "typetransaction_id"
     t.datetime "lastcall"
     t.integer  "user_id"
@@ -76,12 +74,10 @@ ActiveRecord::Schema.define(:version => 20120406075403) do
   end
 
   create_table "properties", :force => true do |t|
-    t.string   "condition"
-    t.string   "value"
-    t.integer  "subject_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string   "typefield"
+    t.string  "condition"
+    t.string  "value"
+    t.integer "subject_id"
+    t.string  "typefield"
   end
 
   create_table "residents", :force => true do |t|
