0fab8d07c3728d3aadad740f783aa54fe011b487
obozrevatel <obozrevatel90@gmail.com>
2012-07-24 21:40:58 +0600

    refactor report controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,50 +15,95 @@ class ReportsController < ApplicationController
   end
   
   def general
-    if params[:period] == "Неделя" and params[:objects] == "Клиенты"
-      render "customers_week"
-    elsif params[:period] == "Месяц" and params[:objects] == "Клиенты"
-      render "customers_month"
-    elsif params[:period] == "Квартал" and params[:objects] == "Клиенты"
-      render "customers_quarter"
-    elsif params[:period] == "Год" and params[:objects] == "Клиенты"
-      render "customers_year"
-    elsif params[:period] == "Неделя" and params[:objects] == "Объекты"
-      render "subjects_week"
-    elsif params[:period] == "Месяц" and params[:objects] == "Объекты"
-      render "subjects_month"
-    elsif params[:period] == "Квартал" and params[:objects] == "Объекты"
-      render "subjects_quarter"
-    elsif params[:period] == "Год" and params[:objects] == "Объекты"
-      render "subjects_year"      
+    if params[:objects] == "Клиенты"
+      case params[:period]
+      when "Неделя"
+        render "customers_week"
+      when "Месяц"
+        render "customers_month"
+      when "Квартал"
+        render "customers_quarter"
+      when "Год"
+        render "customers_year"        
+      end
+    elsif params[:objects] == "Объекты"
+      case params[:period]
+      when "Неделя"
+        render "subjects_week"
+      when "Месяц"
+        render "subjects_month"
+      when "Квартал"
+        render "subjects_quarter"
+      when "Год"
+        render "subjects_year"
+      end      
     end
+    # if params[:period] == "Неделя" and params[:objects] == "Клиенты"
+    #   render "customers_week"
+    # elsif params[:period] == "Месяц" and params[:objects] == "Клиенты"
+    #   render "customers_month"
+    # elsif params[:period] == "Квартал" and params[:objects] == "Клиенты"
+    #   render "customers_quarter"
+    # elsif params[:period] == "Год" and params[:objects] == "Клиенты"
+    #   render "customers_year"
+    # elsif params[:period] == "Неделя" and params[:objects] == "Объекты"
+    #   render "subjects_week"
+    # elsif params[:period] == "Месяц" and params[:objects] == "Объекты"
+    #   render "subjects_month"
+    # elsif params[:period] == "Квартал" and params[:objects] == "Объекты"
+    #   render "subjects_quarter"
+    # elsif params[:period] == "Год" and params[:objects] == "Объекты"
+    #   render "subjects_year"      
+    # end
   end
   
   def activ
-    if params[:period] == "Неделя" #and params[:agent] # [:agent_id] == Report.current(:agent_id)
-      Report.find_user(params[:agent][:agent_id])
+    Report.find_user(params[:agent][:agent_id]) if params[:period]
+    case params[:period]
+    when "Неделя"
       render "activ_week"
-    elsif params[:period] == "Месяц" #and params[:agent_id]
-      Report.find_user(params[:agent][:agent_id])
+    when "Месяц"
       render "activ_month"
-    elsif params[:period] == "Квартал" #and params[:agent_id]
-      Report.find_user(params[:agent][:agent_id])
+    when "Квартал"
       render "activ_quarter"
-    elsif params[:period] == "Год" #and params[:agent_id]
-      Report.find_user(params[:agent][:agent_id])
-      render "activ_year"                      
-    end      
+    when "Год"
+      render "activ_year"    
+    end
+    # if params[:period] == "Неделя" #and params[:agent] # [:agent_id] == Report.current(:agent_id)
+    #   Report.find_user(params[:agent][:agent_id])
+    #   render "activ_week"
+    # elsif params[:period] == "Месяц" #and params[:agent_id]
+    #   Report.find_user(params[:agent][:agent_id])
+    #   render "activ_month"
+    # elsif params[:period] == "Квартал" #and params[:agent_id]
+    #   Report.find_user(params[:agent][:agent_id])
+    #   render "activ_quarter"
+    # elsif params[:period] == "Год" #and params[:agent_id]
+    #   Report.find_user(params[:agent][:agent_id])
+    #   render "activ_year"                      
+    # end      
   end
   
   def finance
-    if params[:period] == "Неделя"
-      render  "weeks"
-    elsif params[:period] == "Месяц"
-      render  "month"
-    elsif params[:period] == "Квартал"
+    case params[:period]
+    when "Неделя"
+      render "weeks"
+    when "Месяц"
+      render "month"      
+    when "Квартал"
       render "quarter"
-    elsif params[:period] == "Год"
-      render "year"      
-    end     
+    when "Год"
+      render "year"
+    end      
+      
+    # if params[:period] == "Неделя"
+    #   render  "weeks"
+    # elsif params[:period] == "Месяц"
+    #   render  "month"
+    # elsif params[:period] == "Квартал"
+    #   render "quarter"
+    # elsif params[:period] == "Год"
+    #   render "year"      
+    # end     
   end
 end
